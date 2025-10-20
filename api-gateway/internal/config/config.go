6f73eff7d47f5305b100d3aa60ae1f4462820509
sorawaslocked <133684349+sorawaslocked@users.noreply.github.com>
2025-10-20 20:09:12 +0500

    Add configuration

--- api-gateway/internal/config/config.go
+++ api-gateway/internal/config/config.go
@@ -1,9 +1,58 @@
 package config
 
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/sorawaslocked/car-rental-api-gateway/internal/pkg/grpc"
+	"os"
+)
+
 type (
+	Config struct {
+		HTTPServer HTTPServer  `yaml:"http_server" env-required:"true"`
+		GRPCServer grpc.Config `yaml:"grpc_server" env-required:"true"`
+		Env        string      `yaml:"env" env-required:"true"`
+	}
+
 	HTTPServer struct {
-		Host    string
-		Port    int
-		GinMode string
+		Host         string `yaml:"host" env:"HTTP_SERVER_HOST" env-required:"true"`
+		Port         int    `yaml:"port" env:"HTTP_SERVER_PORT" env-required:"true"`
+		ReadTimeout  string `yaml:"read_timeout" env:"HTTP_SERVER_READ_TIMEOUT" env-default:"30s"`
+		WriteTimeout string `yaml:"write_timeout" env:"HTTP_SERVER_WRITE_TIMEOUT" env-default:"30s"`
+		IdleTimeout  string `yaml:"idle_timeout" env:"HTTP_SERVER_IDLE_TIMEOUT" env-default:"60s"`
+		GinMode      string `yaml:"gin_mode" env:"GIN_MODE" env-default:"debug"`
 	}
 )
+
+func MustLoad() Config {
+	cfgPath := fetchConfigPath()
+
+	if cfgPath == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(cfgPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + cfgPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(cfgPath, &cfg); err != nil {
+		panic("failed to load config: " + err.Error())
+	}
+
+	return cfg
+}
+
+func fetchConfigPath() string {
+	var path string
+
+	flag.StringVar(&path, "config", "", "config file path")
+	flag.Parse()
+
+	if path == "" {
+		path = os.Getenv("CONFIG_PATH")
+	}
+
+	return path
+}
---
+++ api-gateway/internal/pkg/grpc/grpc.go
@@ -0,0 +1,62 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/keepalive"
+	"time"
+)
+
+type (
+	Config struct {
+		Client Client `yaml:"client" env-required:"true"`
+	}
+
+	Client struct {
+		AuthServiceURL   string        `yaml:"auth_service_url" env:"GRPC_AUTH_SERVICE_URL" env-required:"true"`
+		MaxReceiveSizeMb int           `yaml:"max_receive_size_mb" env:"GRPC_MAX_RECEIVE_SIZE_MB" env-default:"4"`
+		TimeKeepAlive    time.Duration `yaml:"time_keep_alive" env:"GRPC_TIME_KEEP_ALIVE" env-default:"1m"`
+		Timeout          time.Duration `yaml:"timeout" env:"GRPC_TIMEOUT" env-default:"10s"`
+	}
+)
+
+func Connect(target string, clientCfg Client) (*grpc.ClientConn, error) {
+	keepAlivePrms := keepalive.ClientParameters{
+		Time:                clientCfg.TimeKeepAlive,
+		Timeout:             clientCfg.Timeout,
+		PermitWithoutStream: true,
+	}
+
+	maxReceiveSizeBytes := 1024 * 1024 * clientCfg.MaxReceiveSizeMb
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithKeepaliveParams(keepAlivePrms),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(maxReceiveSizeBytes)),
+	}
+
+	return grpc.NewClient(target, opts...)
+}
+
+func PingServer(conn *grpc.ClientConn) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	healthClient := grpc_health_v1.NewHealthClient(conn)
+
+	resp, err := healthClient.Check(ctx, &grpc_health_v1.HealthCheckRequest{
+		Service: "",
+	})
+	if err != nil {
+		return err
+	}
+
+	if resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
+		return errors.New("service is not serving")
+	}
+
+	return nil
+}

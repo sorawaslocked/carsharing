f3cf993bb4fc13dccaf25c1b5d8f93c9e985525c
sorawaslocked <133684349+sorawaslocked@users.noreply.github.com>
2025-10-28 16:50:03 +0500

    Change phone number to be optional

--- user-service/internal/service/helpers.go
+++ user-service/internal/service/helpers.go
@@ -1,55 +1,12 @@
 package service
 
 import (
-	"errors"
-	"github.com/go-playground/validator/v10"
 	"github.com/sorawaslocked/car-rental-user-service/internal/model"
 	"strings"
-	"time"
 )
 
-type registerValidation struct {
-	Email                string    `validate:"required,email"`
-	PhoneNumber          string    `validate:"required,e164"`
-	Password             string    `validate:"required,min=8,max=20,complex_password"`
-	PasswordConfirmation string    `validate:"required,min=8,max=20,eqfield=Password,complex_password"`
-	FirstName            string    `validate:"required,min=1,max=100"`
-	LastName             string    `validate:"required,min=1,max=100"`
-	BirthDate            time.Time `validate:"required,min_age=18"`
-}
-
-type loginValidation struct {
-	Email       string `validate:"required_without=PhoneNumber,omitempty,email"`
-	PhoneNumber string `validate:"required_without=Email,omitempty,e164"`
-	Password    string `validate:"required"`
-}
-
-type refreshTokenValidation struct {
-	RefreshToken string `validate:"required,jwt"`
-}
-
-func validateInput(v *validator.Validate, input any) error {
-	err := v.Struct(input)
-	if err == nil {
-		return nil
-	}
-
-	var errs model.ValidationErrors
-	errs = make(map[string]error)
-	var validationErrors validator.ValidationErrors
-	errors.As(err, &validationErrors)
-
-	for _, fieldErr := range validationErrors {
-		fieldErrField := fieldErr.Field()
-		field := strings.ToLower(fieldErrField[:1]) + fieldErrField[1:]
-
-		if _, ok := errs[field]; ok {
-			continue
-		}
-		errs[field] = validationError(fieldErr)
-	}
-
-	return errs
+func uncapitalize(s string) string {
+	return strings.ToLower(s[:1]) + s[1:]
 }
 
 func toRoleStrings(roles []model.Role) []string {
--- user-service/migrations/000002_create_users_table.up.sql
+++ user-service/migrations/000002_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     email citext NOT NULL UNIQUE,
-    phone_number VARCHAR(20) NOT NULL UNIQUE,
+    phone_number VARCHAR(20) UNIQUE,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     birth_date DATE NOT NULL,

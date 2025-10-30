f2a991bd07af5201c17a99cd3840cf172a1c5dd4
sorawaslocked <133684349+sorawaslocked@users.noreply.github.com>
2025-10-31 03:22:45 +0500

    Change name of the logger variable

--- api-gateway/internal/adapter/http/middleware.go
+++ api-gateway/internal/adapter/http/middleware.go
@@ -8,18 +8,18 @@ import (
 
 func LoggerMiddleware(log *slog.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		requestLog := log.With(
+		logger := log.With(
 			slog.String("requestId", requestid.Get(c)),
 			slog.String("clientIP", c.ClientIP()),
 			slog.String("method", c.Request.Method),
 			slog.String("path", c.Request.URL.Path),
 			slog.String("query", c.Request.URL.RawQuery),
 		)
-		requestLog.Info("api request")
+		logger.Info("api request")
 
 		c.Next()
 
-		requestLog.Info(
+		logger.Info(
 			"api response",
 			slog.Int("status", c.Writer.Status()),
 		)

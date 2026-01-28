3ba9e961e147171d4ac09a0b700ccd9862cb1b15
sorawaslocked <133684349+sorawaslocked@users.noreply.github.com>
2026-01-28 09:13:43 +0500

    Fix jwt interface

--- user-service/internal/adapter/grpc/interceptor/interface.go
+++ user-service/internal/adapter/grpc/interceptor/interface.go
@@ -1,7 +1,9 @@
 package interceptor
 
+import "time"
+
 type JwtProvider interface {
-	GenerateAccessToken(id uint64, roles []string) (string, error)
-	GenerateRefreshToken(id uint64, roles []string) (string, error)
+	GenerateAccessToken(id uint64, roles []string) (string, time.Time, error)
+	GenerateRefreshToken(id uint64, roles []string) (string, time.Time, error)
 	VerifyAndParseClaims(token string) (uint64, []string, error)
 }
